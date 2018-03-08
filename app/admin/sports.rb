6916a85aa7dc29d118cb3ce40f2e38b3c37af7b3
Raghu Betina <raghu@firstdraft.com>
2018-03-08 19:43:36 +0000

    Whitelist columns in Sport for ActiveAdmin

--- app/admin/sports.rb
+++ app/admin/sports.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Sport do
+
+ permit_params :spot_id, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
