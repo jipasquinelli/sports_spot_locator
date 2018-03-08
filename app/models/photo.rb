49112f6d8bae833a37eb132a6f41dfa2976b38f1
Raghu Betina <raghu@firstdraft.com>
2018-03-08 19:42:55 +0000

    Direct association between spots and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :spot
+
   belongs_to :sport,
              :required => false
 
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,9 @@
 class Spot < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :dependent => :destroy
+
   has_many   :sports,
              :dependent => :nullify
 
